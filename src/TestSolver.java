1f5e5ba591326778434da83b0047afda0c9c5069
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-09 14:58:35 +0100

    Nu är all hierarki fixad

---
+++ src/TestSolver.java
@@ -0,0 +1,53 @@
+package src;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestSolver {
+
+    @BeforeEach
+    void solve(){
+
+    }
+
+    @AfterEach
+    void legal(){
+      
+    }
+
+    @Test
+    void set(){
+   
+    }
+
+    @Test
+    void remove(){
+       
+    }
+
+    @Test
+    void clear(){
+       
+    }
+
+    @Test
+    void setMatrix(){
+
+        // for(int i = 0; i < rows; i++){
+        //     for(int j = 0; j < cols; j++){
+        //         System.out.println(matrix[i][j]);
+        //     }
+        // }
+
+    }
+
+    @Test
+    void getMatrix(){
+
+    }
+    
+}
