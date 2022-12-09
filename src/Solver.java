a64cd1461d0a780c6871c419b54cc86f1cf65d64
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-09 14:41:05 +0100

    Init

--- src/Solver.java
+++ src/Solver.java
@@ -1,5 +1,64 @@
 package src;
 
-public class Solver {
+import java.util.Arrays;
+
+
+public class Solver implements SudokuSolver {
+
+    public Solver(){
+
+    }
+
+    @Override
+    public boolean solve() {    //svår
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean legal(int digit, int row, int col) { //svår
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void set(int row, int col, int digit) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void remove(int row, int col) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(getMatrix(), 0);
+
+       /** int rows = matrix.length;
+        int cols = matrix[0].length;
+
+            for(int i = 0; i < rows; i++){
+                for(int j = 0; j < cols; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+            */
+    }
+
+    @Override
+    public void setMatrix(int[][] matrix) {
+        matrix = new int[9][9];
+        
+    }
+
+    @Override
+    public int[][] getMatrix() {
+        // TODO Auto-generated method stub
+        return null;
+    } 
     
+
 }
