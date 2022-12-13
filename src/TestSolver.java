97479dfe076ccbee644edf547fab5c7f808b2783
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-14 00:29:27 +0100

    Final

--- src/TestSolver.java
+++ src/TestSolver.java
@@ -27,20 +27,13 @@ public class TestSolver {
         s = new Solver();
     }
 
-
     @AfterEach
     void tearDown(){
         s = null;
     }
 
-
     @Test
-    void solveSolveablePuzzle(){
-
-    }
-
-    @Test
-    void solveEmptyPuzzle(){
+    void testSolveEmptyPuzzle(){
         int[][] emptyMatrix = {
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -52,7 +45,9 @@ public class TestSolver {
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0}
         };
+        
     s.setMatrix(emptyMatrix);
+    //Ensure puzzle is empty and check if solveable.
     int [][] matrix = s.getMatrix();
     for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
@@ -61,36 +56,100 @@ public class TestSolver {
         }
     
     assertTrue(s.solve(), "Method returns true when sudoku is solved.");
-
+    
+    //Ensure all values in matrix puzzle is legal.
     for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 assertTrue(s.legal(matrix[i][j], i, j), "The matrix is not solved correctly.");
             }
         }
     }
+
+    @Test
+    void testSolveSolveablePuzzle(){
+        int[][] examplePuzzle = {
+            {0, 0, 8, 0, 0, 9, 0, 6, 2},
+            {0, 0, 0, 0, 0, 0, 0, 0, 5},
+            {1, 0, 2, 5, 0, 0, 0, 0, 0},
+            {0, 0, 0, 2, 1, 0, 0, 9, 0},
+            {0, 5, 0, 0, 0, 0, 6, 0, 0},
+            {6, 0, 0, 0, 0, 0, 0, 2, 8},
+            {4, 1, 0, 6, 0, 8, 0, 0, 0},
+            {8, 6, 0, 0, 3, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 4, 0, 0}
+        };
+        
+        int[][] solvedExamplePuzzle = {
+            {5, 4, 8, 1, 7, 9, 3, 6, 2},
+            {3, 7, 6, 8, 2, 4, 9, 1, 5},
+            {1, 9, 2, 5, 6, 3, 8, 7, 4},
+            {7, 8, 4, 2, 1, 6, 5, 9, 3},
+            {2, 5, 9, 3, 8, 7, 6, 4, 1},
+            {6, 3, 1, 9, 4, 5, 7, 2, 8},
+            {4, 1, 5, 6, 9, 8, 2, 3, 7},
+            {8, 6, 7, 4, 3, 2, 1, 5, 9},
+            {9, 2, 3, 7, 5, 1, 4, 8, 6}
+        }; 
+        s.setMatrix(examplePuzzle);
+        assertTrue(s.solve(), "Should return true if solved.");
+        
+        //Crosscheck matrices.
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                assertEquals(examplePuzzle[i][j], solvedExamplePuzzle[i][j], "The input matrix does not match the solved matrix.");
+            }
+        }
+    }
     
     @Test
-    void unsolveablePuzzle(){
+    void testUnsolveablePuzzle(){
+        int[][] unsolveableMatrix = {
+            {1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        s.setMatrix(unsolveableMatrix);
+        assertFalse(s.solve(), "Should not be solveable");  
+
+    }
+
+    @Test
+    void testLegal(){
+        int[][] testMatrix = {
+            {0, 1, 0, 2, 0, 0, 3, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 3, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 2, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        s.setMatrix(testMatrix);
+
+        assertTrue(s.legal(1,0,1), "Element is legal but not allowed to be placed.");
+        assertTrue(s.legal(2,0,3), "Element is legal but not allowed to be placed.");
+        assertTrue(s.legal(3,0,6), "Element is legal but not allowed to be placed.");
+        assertTrue(s.legal(1,3,0), "Element is legal but not allowed to be placed.");
+        assertTrue(s.legal(3,3,3), "Element is legal but not allowed to be placed.");
+        assertTrue(s.legal(2,6,6), "Element is legal but not allowed to be placed.");
+
+        assertFalse(s.legal(0,0,1), "Element is not legal but allowed to be placed.");
+        assertFalse(s.legal(2,5,6), "Element is not legal but allowed to be placed.");
+        assertFalse(s.legal(2,6,5), "Element is not legal but allowed to be placed.");
         
+
     }
 
     @Test
-    void legal(){
-
-        // assertTrue(s.legal(1,0,1), "Element is legal but not allowed to be placed.");
-        // assertTrue(s.legal(2,0,3), "Element is legal but not allowed to be placed.");
-        // assertTrue(s.legal(3,0,6), "Element is legal but not allowed to be placed.");
-        // assertTrue(s.legal(1,3,0), "Element is legal but not allowed to be placed.");
-        // assertTrue(s.legal(2,3,3), "Element is legal but not allowed to be placed.");
-        // assertTrue(s.legal(3,6,6), "Element is legal but not allowed to be placed.");
-
-        // assertFalse(s.legal(5,0,1), "Element is not legal but allowed to be placed.");
-        // assertFalse(s.legal(3,0,1), "Element is not legal but allowed to be placed.");
-        // assertFalse(s.legal(8,8,8), "Element is not legal but allowed to be placed.");
-    }
-
-    @Test
-    void set(){
+    void testSet(){
         //Sets all values for the row position in each column of current row (All columns in Row 1 = 1, Row 2 = 2, etc.)
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
@@ -134,7 +193,7 @@ public class TestSolver {
     }
 
     @Test
-    void clear(){
+    void testClear(){
     s.setMatrix(defaultMatrix);  
     int[][] matrix = s.getMatrix();
        
@@ -154,7 +213,7 @@ public class TestSolver {
     }
 
     @Test
-    void setMatrix() {
+    void testSetMatrix() {
         int[][] puzzle = {
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -180,26 +239,29 @@ public class TestSolver {
 
     @Test
     void testGetMatrix(){
-        // int [][] matrixFromSolver = s.getMatrix();
-
-        // for(int i = 0; i < 9; i++){
-        //     for(int j = 0; j < 9; j++){
-        //         assertEquals(defaultMatrix[i][j], matrixFromSolver[i][j]);
-        //     }
-        // }
-
-        // for(int i = 0; i < 9; i++){
-        //     for(int j = 0; j < 9; j++){
-        //         s.set(i, j, 1);
-        //     }
-        // }
-        // matrixFromSolver = s.getMatrix();
+        s.setMatrix(defaultMatrix);
+        int [][] matrixFromSolver = s.getMatrix();
+        //Ensure puzzle is copied from solver.
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                assertEquals(defaultMatrix[i][j], matrixFromSolver[i][j]);
+            }
+        }
+        //Set all values to 1 in solver puzzle.
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                s.set(i, j, 1);
+            }
+        }
+        //Copy again.
+        matrixFromSolver = s.getMatrix();
         
-        // for(int i = 0; i < 9; i++){
-        //     for(int j = 0; j < 9; j++){
-        //         assertEquals(1, matrixFromSolver[i][j]);
-        //     }
-        // }
+        //Check copied puzzle if same values as solver.
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                assertEquals(1, matrixFromSolver[i][j]);
+            }
+        }
 
 
     }
